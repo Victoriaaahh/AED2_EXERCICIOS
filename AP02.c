0c03066ffda73af099a4df41229d3a0f706cb5ff
Victoria Ayumi <victoriaayumikose@gmail.com>
2026-04-08 15:34:49 -0300

    novo

--- AP02.c
+++ AP02.c
@@ -1,52 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXN 100005
+#define MAX 100000 //10^5
 
-double V[MAXN];
-long long count = 0;
+double V[MAX];
+long long cont = 0;
 
-void merge(int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    double L[MAXN], R[MAXN];
-    for(int i = 0; i < n1; i++) L[i] = V[left + i];
-    for(int i = 0; i < n2; i++) R[i] = V[mid + 1 + i];
+//conta quantos pares (i, j) existem (i < j e V[i] > 2 * V[j]) 
+void merge(int esq, int meio, int dir) {
+    int n1 = meio - esq + 1;
+    int n2 = dir - meio;
+    double E[MAX], D[MAX];
+    for(int i = 0; i < n1; i++) E[i] = V[esq + i];
+    for(int i = 0; i < n2; i++) D[i] = V[meio + 1 + i];
     
-    int p = 0;
-    for(int jj = 0; jj < n2; jj++) {
-        while(p < n1 && L[p] <= 2 * R[jj]) p++;
-        count += n1 - p;
+    int t = 0;
+    for(int i = 0; i < n2; i++) {
+        while(t < n1 && E[t] <= 2 * D[i]) t++;
+        cont += n1 - t;
     }
     
-    int i = 0, j = 0, k = left;
-    while(i < n1 && j < n2) {
-        if(L[i] <= R[j]) {
-            V[k++] = L[i++];
+    int ii = 0, j = 0, k = esq;
+    while(ii < n1 && j < n2) {
+        if(E[ii] <= D[j]) {
+            V[k++] = E[ii++];
         } else {
-            V[k++] = R[j++];
+            V[k++] = D[j++];
         }
     }
-    while(i < n1) V[k++] = L[i++];
-    while(j < n2) V[k++] = R[j++];
+    while(ii < n1) V[k++] = E[ii++];
+    while(j < n2) V[k++] = D[j++];
 }
 
-void mergeSort(int left, int right) {
-    if(left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(left, mid);
-        mergeSort(mid + 1, right);
-        merge(left, mid, right);
+//merge sort
+void mergeSort(int esq, int dir) {
+    if(esq < dir) {
+        int meio = esq + (dir - esq) / 2;
+        mergeSort(esq, meio);
+        mergeSort(meio + 1, dir);
+        merge(esq, meio, dir);
     }
 }
 
 int main() {
     int N;
     scanf("%d", &N);
-    for(int i = 0; i < N; i++) scanf("%lf", &V[i]);
-    count = 0;
+    for(int i = 0; i < N; i++){ 
+        scanf("%lf", &V[i]);
+    }
+    cont = 0;
     mergeSort(0, N - 1);
-    printf("%lld\n", count);
+    printf("%lld\n", cont);
     for(int i = 0; i < N; i++) {
         printf("%.2f ", V[i]);
     }
